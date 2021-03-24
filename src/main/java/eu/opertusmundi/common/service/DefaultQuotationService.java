0e58cb97910279684e9a8e879abdade3419ba13c
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-03-24 13:45:32 +0200

    [WIP] Add quotation service
    Added service for computing effective pricing models.

---
+++ src/main/java/eu/opertusmundi/common/service/DefaultQuotationService.java
@@ -0,0 +1,113 @@
+package eu.opertusmundi.common.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import eu.opertusmundi.common.model.catalogue.client.CatalogueItemDto;
+import eu.opertusmundi.common.model.pricing.BasePricingModelCommandDto;
+import eu.opertusmundi.common.model.pricing.EffectivePricingModelDto;
+import eu.opertusmundi.common.model.pricing.QuotationException;
+import eu.opertusmundi.common.model.pricing.QuotationMessageCode;
+import eu.opertusmundi.common.model.pricing.QuotationParametersDto;
+import eu.opertusmundi.common.model.pricing.QuotationParametersDto.SystemParameters;
+
+@Service
+public class DefaultQuotationService implements QuotationService {
+
+    // TODO: Set from configuration
+    private final BigDecimal tax = new BigDecimal(24);
+    
+    @Override
+    public void validate(BasePricingModelCommandDto model) throws QuotationException {
+        model.validate();
+    }
+    
+    @Override
+    public void validate(BasePricingModelCommandDto model, QuotationParametersDto params) throws QuotationException {
+        model.validate(params);
+    }
+
+    @Override
+    public EffectivePricingModelDto createQuotation(
+        CatalogueItemDto asset, UUID pricingModelKey, QuotationParametersDto params
+    ) throws QuotationException {
+        try {
+            final BasePricingModelCommandDto pricingModel = asset.getPricingModels().stream()
+                .filter(m -> m.getKey().equals(pricingModelKey))
+                .findFirst()
+                .orElse(null);
+            
+            if(pricingModel == null) {
+                throw new QuotationException(
+                    QuotationMessageCode.PRICING_MODEL_NOT_FOUND,
+                    String.format("Pricing model [%s] not found", pricingModelKey)
+                );
+            }
+            
+            // Inject required parameters
+            this.injectParameters(pricingModel, params);
+            
+            // Validate parameters
+            this.validate(pricingModel, params);
+            
+            return pricingModel.compute(params);
+        } catch(QuotationException ex) {
+            throw ex;
+        } catch(Exception ex) {
+            throw new QuotationException("Quotation failed", ex);
+        }
+    }
+
+    @Override
+    public List<EffectivePricingModelDto> createQuotation(CatalogueItemDto asset) throws QuotationException {
+        return asset.getPricingModels().stream()
+            .map(m -> {
+                final QuotationParametersDto params = new QuotationParametersDto();
+
+                // Inject required parameters
+                this.injectParameters(m, params);
+
+                // Compute default quotations without parameters. No
+                // parameter validation is required. Some pricing model may
+                // return empty result
+                return m.compute(params);
+            })
+            .collect(Collectors.toList());
+    }
+    
+    /**
+     * Injects dynamic parameters to a quotation command object e.g. row count
+     * or population
+     * 
+     * @param command
+     */
+    private void injectParameters(BasePricingModelCommandDto command, QuotationParametersDto params) {
+        params.setTaxPercent(tax);
+
+        switch (command.getType()) {
+            case FIXED_PER_ROWS :
+                // TODO: Compute rows based on NUTS codes
+                if (params.getNuts() != null && params.getNuts().size() > 0) {
+                    params.setSystemParams(SystemParameters.fromRows(10000L));
+                }
+                break;
+
+            case FIXED_FOR_POPULATION :
+                // TODO: Compute population based on NUTS codes
+                if (params.getNuts() != null && params.getNuts().size() > 0) {
+                    params.setSystemParams(SystemParameters.fromPopulation(500000L, 20));
+                }
+                
+                break;
+
+            default :
+                // No operation
+                break;
+        }
+    }
+    
+}
---
+++ src/main/java/eu/opertusmundi/common/service/QuotationService.java
@@ -0,0 +1,60 @@
+package eu.opertusmundi.common.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import eu.opertusmundi.common.model.catalogue.client.CatalogueItemDto;
+import eu.opertusmundi.common.model.pricing.BasePricingModelCommandDto;
+import eu.opertusmundi.common.model.pricing.EffectivePricingModelDto;
+import eu.opertusmundi.common.model.pricing.QuotationException;
+import eu.opertusmundi.common.model.pricing.QuotationParametersDto;
+
+public interface QuotationService {
+
+    /**
+     * Validates the specified pricing model
+     * 
+     * @param model
+     * @param params
+     * @throws QuotationException
+     */
+    void validate(BasePricingModelCommandDto model) throws QuotationException;
+    
+    /**
+     * Validates parameters for the specified pricing model
+     * 
+     * @param model
+     * @param params
+     * @throws QuotationException
+     */
+    void validate(BasePricingModelCommandDto model, QuotationParametersDto params) throws QuotationException;
+    
+    /**
+     * Computes a quotation
+     * 
+     * Ignores command's asset PID parameter and computes quotation based on the
+     * specified asset
+     * 
+     * @param asset
+     * @param pricingModelKey
+     * @param params
+     * @return
+     * @throws QuotationException
+     */
+    EffectivePricingModelDto createQuotation(
+        CatalogueItemDto asset, UUID pricingModelKey, QuotationParametersDto params
+    ) throws QuotationException;
+    
+    /**
+     * Computes quotations for all asset pricing models
+     * 
+     * For a pricing model that requires parameters, an empty effect pricing model is returned
+     *  
+     * @param asset
+     * @param command
+     * @return
+     * @throws QuotationException
+     */
+    List<EffectivePricingModelDto> createQuotation(CatalogueItemDto asset) throws QuotationException;
+    
+}
