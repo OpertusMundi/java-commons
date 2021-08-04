ad161ec39af4fadd45167f9b0a264ff23d45d2a7
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-08-04 06:58:21 +0300

    Fix Hibernate warning

--- src/main/java/eu/opertusmundi/common/model/contract/ContractSectionOptionDto.java
+++ src/main/java/eu/opertusmundi/common/model/contract/ContractSectionOptionDto.java
@@ -2,29 +2,29 @@ package eu.opertusmundi.common.model.contract;
 
 import java.util.List;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class ContractSectionOptionDto{
-	
+
 	@Getter
 	@Setter
     private String body;
-	
+
 	@Getter
 	@Setter
     private String bodyHtml;
-	
+
 	@Getter
 	@Setter
     private List<ContractSectionSubOptionDto> subOptions;
-	
+
 	@Getter
 	@Setter
 	private String summary;
-	
+
 	@Getter
 	@Setter
 	private String icon;
