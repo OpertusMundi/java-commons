507a2a6dfb1375fe4d35d2e33d493163cfa39d51
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-08-19 14:20:53 +0300

    Update EPSG code repository
    Added method for searching active EPSG codes by name and code.

--- src/main/java/eu/opertusmundi/common/repository/EpsgRepository.java
+++ src/main/java/eu/opertusmundi/common/repository/EpsgRepository.java
@@ -1,8 +1,11 @@
 package eu.opertusmundi.common.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,5 +17,12 @@ public interface EpsgRepository extends JpaRepository<EpsgEntity, Integer> {
 
     @Override
     Page<EpsgEntity> findAll(Pageable page);
+
+    @Query("SELECT   e FROM Epsg e "
+         + "WHERE    (:name is null or e.name like :name) and "
+         + "         (:code is null or CAST(e.code as text) like :code) and "
+         + "         (e.active = true) "
+         + "ORDER BY e.name")
+    List<EpsgEntity> findAllActive(String name, String code);
 
 }
