71a86120247de21a94cef93a472041f7ab8e6d65
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2021-09-03 14:02:01 +0300

    Add profileName to e.o.c.model.jupyter.server.ServerDto

--- src/main/java/eu/opertusmundi/common/model/jupyter/server/ServerDto.java
+++ src/main/java/eu/opertusmundi/common/model/jupyter/server/ServerDto.java
@@ -2,6 +2,7 @@ package eu.opertusmundi.common.model.jupyter.server;
 
 import java.net.URL;
 import java.time.ZonedDateTime;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -14,28 +15,41 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ServerDto 
 {
-	private String name;
-	
-	private boolean ready;
-	
-	@JsonIgnore
-	private ActionType pendingAction;
-	
-	@JsonSetter("pending")
-	public void setPendingActionFromString(String name)
-	{
-		this.pendingAction = ActionType.fromName(name);
-	}
-	
-	/**
-	 * A URL path (relative to JupyterHub root URL) for the user's server
-	 */
-	@JsonProperty("url")
-	private String urlPath;
-	
-	@JsonProperty("started")
-	private ZonedDateTime startedAt;
-	
-	@JsonProperty("last_activity")
-	private ZonedDateTime lastActivityAt;
+    private String name;
+    
+    private boolean ready;
+    
+    @JsonIgnore
+    private ActionType pendingAction;
+    
+    @JsonSetter("pending")
+    public void setPendingActionFromString(String name)
+    {
+        this.pendingAction = ActionType.fromName(name);
+    }
+    
+    /**
+     * A URL path (relative to JupyterHub root URL) for the user's server
+     */
+    @JsonProperty("url")
+    private String urlPath;
+    
+    @JsonProperty("started")
+    private ZonedDateTime startedAt;
+    
+    @JsonProperty("last_activity")
+    private ZonedDateTime lastActivityAt;
+    
+    @JsonIgnore
+    private String profileName;
+    
+    @JsonSetter("user_options")
+    public void setFromUserOptions(Map<String,String> userOptions)
+    {
+        if (userOptions == null) {
+            return;
+        }
+        
+        this.profileName = userOptions.get("profile");
+    }
 }
