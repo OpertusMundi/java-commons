d12282adb17ed20372fc080b0d1f847c3b62dadc
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-11-17 15:55:06 +0200

    Conditional activation of Sentinel Hub integration

--- src/main/java/eu/opertusmundi/common/service/integration/DefaultSentinelHubService.java
+++ src/main/java/eu/opertusmundi/common/service/integration/DefaultSentinelHubService.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
@@ -29,6 +30,7 @@ import feign.codec.Decoder;
 import feign.codec.Encoder;
 
 @Service
+@ConditionalOnProperty(name = "opertusmundi.sentinel-hub.enabled")
 @Import(FeignClientsConfiguration.class)
 public class DefaultSentinelHubService implements SentinelHubService {
 
