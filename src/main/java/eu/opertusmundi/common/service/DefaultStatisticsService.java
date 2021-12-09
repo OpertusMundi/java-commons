c615d4fdd190f743b4aae69840752e7d172fd421
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-12-09 11:26:57 +0200

    Fix several issues in statistics service
    Fixed publication date parsing.
    Fixed NullPointerException when enumerating pricing models.
    Fixed mixed SRID error in SQL statement.

--- src/main/java/eu/opertusmundi/common/service/DefaultStatisticsService.java
+++ src/main/java/eu/opertusmundi/common/service/DefaultStatisticsService.java
@@ -1,19 +1,24 @@
 package eu.opertusmundi.common.service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.locationtech.jts.geom.Geometry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import eu.opertusmundi.common.domain.CountryEntity;
+import eu.opertusmundi.common.domain.CountryEuropeEntity;
 import eu.opertusmundi.common.model.analytics.AssetStatisticsCommandDto;
 import eu.opertusmundi.common.model.analytics.AssetStatisticsDto;
 import eu.opertusmundi.common.model.catalogue.client.CatalogueItemDto;
 import eu.opertusmundi.common.model.catalogue.client.EnumTopicCategory;
 import eu.opertusmundi.common.model.pricing.EffectivePricingModelDto;
+import eu.opertusmundi.common.model.pricing.QuotationDto;
+import eu.opertusmundi.common.model.spatial.CountryEuropeDto;
 import eu.opertusmundi.common.repository.AssetStatisticsRepository;
 import eu.opertusmundi.common.repository.CountryRepository;
 import eu.opertusmundi.common.util.StreamUtils;
@@ -34,20 +39,30 @@ public class DefaultStatisticsService implements StatisticsService{
     public AssetStatisticsDto updateStatisticsPublishAsset(String pid) {
     	AssetStatisticsCommandDto assetStatisticsCommandDto = new AssetStatisticsCommandDto();
     	
-    	CatalogueItemDto cItem = this.catalogueService.findOne(null, pid, null, false); 
+    	CatalogueItemDto item = this.catalogueService.findOne(null, pid, null, false); 
+    	
+        final Geometry                       geom            = item.getGeometry();
+        final EnumTopicCategory              segment         = StreamUtils.from(item.getTopicCategory()).findFirst().orElse(null);
+        final ZonedDateTime                  publicationDate = LocalDateTime.parse(item.getPublicationDate()).atZone(ZoneId.of("UTC"));
 
-        final Geometry geom 				= cItem.getGeometry();
-        final List<CountryEntity> countries = this.countryRepository.getCountriesWithinGeometry(geom);
-        final EnumTopicCategory segment		= StreamUtils.from(cItem.getTopicCategory()).findFirst().orElse(null);    
-        final ZonedDateTime publicationDate = ZonedDateTime.parse(cItem.getPublicationDate());
-        final List<EffectivePricingModelDto> pricingModel = cItem.getEffectivePricingModels();
-        
         BigDecimal maxPrice = new BigDecimal(0);
-        for (int i = 0 ; i < pricingModel.size() ; i++) {
-        	if (cItem.getEffectivePricingModels().get(i).getQuotation().getTotalPriceExcludingTax().compareTo(maxPrice) == 1) {
-        		maxPrice = cItem.getEffectivePricingModels().get(i).getQuotation().getTotalPriceExcludingTax();
-        	}
+        for (final EffectivePricingModelDto p : item.getEffectivePricingModels()) {
+            final QuotationDto quotation = p.getQuotation();
+            if (quotation == null) {
+                continue;
+            }
+            if (quotation.getTotalPriceExcludingTax().compareTo(maxPrice) == 1) {
+                maxPrice = quotation.getTotalPriceExcludingTax();
+            }
         }
+
+        // Must set SRID to geometry before executing the query
+        if (geom.getSRID() == 0) {
+            geom.setSRID(4326);
+        }
+        final List<CountryEuropeDto> countries = this.countryRepository.getCountriesWithinGeometry(geom).stream()
+            .map(CountryEuropeEntity::toDto)
+            .collect(Collectors.toList());
         
         assetStatisticsCommandDto.setPid(pid);
         assetStatisticsCommandDto.setSegment(segment);
