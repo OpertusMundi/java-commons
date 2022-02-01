3ac3c6010a72f2b85e78310d210f770ab62e39d7
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2022-02-01 22:06:30 +0200

    Let DirectoryTraverse filter entries based on given predicate
    The predicate can be specific to the current file-naming context

--- src/main/java/eu/opertusmundi/common/service/DirectoryTraverse.java
+++ src/main/java/eu/opertusmundi/common/service/DirectoryTraverse.java
@@ -2,35 +2,36 @@ package eu.opertusmundi.common.service;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.function.Predicate;
 
 import eu.opertusmundi.common.model.file.DirectoryDto;
 
 public interface DirectoryTraverse
 {
     /**
-     * @see {@link DirectoryTraverse#getDirectoryInfo(Path, int)}
+     * @see DirectoryTraverse#getDirectoryInfo(Path, int, Predicate)
      */
     DirectoryDto getDirectoryInfo(Path rootDir) throws IOException;
 
+    /**
+     * @see DirectoryTraverse#getDirectoryInfo(Path, int, Predicate)
+     */
+    DirectoryDto getDirectoryInfo(Path rootDir, int maxDepth) throws IOException;
+
+    /**
+     * @see DirectoryTraverse#getDirectoryInfo(Path, int, Predicate)
+     */
+    DirectoryDto getDirectoryInfo(Path rootDir, Predicate<String> namePredicate) throws IOException;
+    
     /**
      * Traverse directory entries (recursively) and collect detailed information on
      * file-system entries (files and nested directories).
      *
      * @param rootDir The root directory of this traversal
      * @param maxDepth A maximum depth to descend
+     * @param namePredicate A predicate that filters names to be included (directories or files)
      * @throws IOException
      */
-    DirectoryDto getDirectoryInfo(Path rootDir, int maxDepth) throws IOException;
-
-    /**
-     * Traverse directory entries (recursively) and collect detailed information on
-     * file-system entries (files and nested directories).
-     *
-     * @param rootDir The root directory of this traversal
-     * @param exclude List of folder names to exclude
-     * @throws IOException
-     */
-    DirectoryDto getDirectoryInfo(Path rootDir, List<String> exclude) throws IOException;
+    DirectoryDto getDirectoryInfo(Path rootDir, int maxDepth, Predicate<String> namePredicate) throws IOException;
 
 }
