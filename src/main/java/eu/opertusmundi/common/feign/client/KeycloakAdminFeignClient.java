2b759a1d964dae30513f585f62850ecae01aae83
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-03-14 14:19:42 +0200

    Update Keycloak Feign client
    Enabled Feign client conditionally based on property
    `opertusmundi.feign.keycloak.url`.
    Updated client configuration to support
    `application/x-www-form-urlencoded` forms.

--- src/main/java/eu/opertusmundi/common/feign/client/KeycloakAdminFeignClient.java
+++ src/main/java/eu/opertusmundi/common/feign/client/KeycloakAdminFeignClient.java
@@ -3,6 +3,7 @@ package eu.opertusmundi.common.feign.client;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.http.HttpHeaders;
@@ -17,16 +18,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import eu.opertusmundi.common.feign.client.config.KeycloakAdminFeignClientConfiguration;
-import eu.opertusmundi.common.model.keycloak.server.*;
+import eu.opertusmundi.common.model.keycloak.server.CredentialDto;
+import eu.opertusmundi.common.model.keycloak.server.GroupDto;
+import eu.opertusmundi.common.model.keycloak.server.GroupQueryDto;
+import eu.opertusmundi.common.model.keycloak.server.PageRequest;
+import eu.opertusmundi.common.model.keycloak.server.RefreshTokenForm;
+import eu.opertusmundi.common.model.keycloak.server.RefreshTokenResponse;
+import eu.opertusmundi.common.model.keycloak.server.UserDto;
+import eu.opertusmundi.common.model.keycloak.server.UserQueryDto;
 
 
 /**
  * A Feign client for the Keycloak Admin REST API.
  * <p>
  * Note: This client supports only a small part of the (v15.0) Admin API.
- * 
+ *
  * @see https://www.keycloak.org/docs-api/15.0/rest-api/index.html
  */
+@ConditionalOnProperty("opertusmundi.feign.keycloak.url")
 @FeignClient(
     value = "keycloakAdminClient",
     url = "${opertusmundi.feign.keycloak.url}",
@@ -36,30 +45,30 @@ public interface KeycloakAdminFeignClient
     /**
      * Refresh token and obtain a new access token.
      * <p>
-     * Note that this method is not technically part of the Admin API 
+     * Note that this method is not technically part of the Admin API
      * (is part of the OAuth2 spec for refreshing an access token if holding a refresh token).
-     * 
+     *
      * @param realm
      * @param refreshTokenForm
-     * 
+     *
      * @see https://datatracker.ietf.org/doc/html/rfc6749#section-6
      * @see https://github.com/keycloak/keycloak-documentation/blob/main/server_admin/topics/sessions/offline.adoc
      */
     @PostMapping(
         path = "realms/{realm}/protocol/openid-connect/token",
-        produces = MediaType.APPLICATION_JSON_VALUE, 
+        produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     RefreshTokenResponse refreshToken(
-        @PathVariable("realm") String realm, 
+        @PathVariable("realm") String realm,
         RefreshTokenForm refreshTokenForm);
-    
+
     //
     // Admin API
     //
-    
+
     /**
      * Find users
-     * 
+     *
      * @param realm
      * @param authorizationHeader
      * @param query
@@ -70,12 +79,12 @@ public interface KeycloakAdminFeignClient
         produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<UserDto>> findUsers(
         @PathVariable("realm") String realm,
-        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, 
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @SpringQueryMap UserQueryDto query);
-    
+
     /**
      * Count users
-     * 
+     *
      * @param realm
      * @param authorizationHeader
      * @param query
@@ -86,12 +95,12 @@ public interface KeycloakAdminFeignClient
         produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Integer> countUsers(
         @PathVariable("realm") String realm,
-        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, 
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @SpringQueryMap UserQueryDto query);
-    
+
     /**
      * Get user by ID
-     * 
+     *
      * @param realm
      * @param userId
      * @param authorizationHeader
@@ -104,10 +113,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("realm") String realm,
         @PathVariable("userId") UUID userId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
-    
+
     /**
      * Create user. The URI for the newly created user is returned a {@code location} response header.
-     * 
+     *
      * @param realm
      * @param authorizationHeader
      * @param user
@@ -121,10 +130,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("realm") String realm,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @RequestBody UserDto user);
-    
+
     /**
      * Update user
-     * 
+     *
      * @param realm
      * @param userId
      * @param authorizationHeader
@@ -134,17 +143,17 @@ public interface KeycloakAdminFeignClient
      */
     @PutMapping(
         path = "admin/realms/{realm}/users/{userId}",
-        produces = MediaType.APPLICATION_JSON_VALUE, 
+        produces = MediaType.APPLICATION_JSON_VALUE,
         consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Void> updateUser(
         @PathVariable("realm") String realm,
         @PathVariable("userId") UUID userId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @RequestBody UserDto user);
-    
+
     /**
      * Delete user
-     * 
+     *
      * @param realm
      * @param userId
      * @param authorizationHeader
@@ -157,10 +166,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("realm") String realm,
         @PathVariable("userId") UUID userId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
-    
+
     /**
      * Reset password for a user
-     * 
+     *
      * @param realm
      * @param userId
      * @param authorizationHeader
@@ -170,13 +179,13 @@ public interface KeycloakAdminFeignClient
     @PutMapping(
         path = "admin/realms/{realm}/users/{userId}/reset-password",
         produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE) 
+        consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Void> resetPasswordForUser(
         @PathVariable("realm") String realm,
         @PathVariable("userId") UUID userId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @RequestBody CredentialDto cred);
-    
+
     @GetMapping(
         path = "admin/realms/{realm}/groups",
         produces = MediaType.APPLICATION_JSON_VALUE)
@@ -184,10 +193,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("realm") String realm,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @SpringQueryMap GroupQueryDto query);
-    
+
     /**
      * Get a group
-     * 
+     *
      * @param realm
      * @param groupId
      * @param authorizationHeader
@@ -200,10 +209,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("realm") String realm,
         @PathVariable("groupId") UUID groupId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
-    
+
     /**
-     * Get members (users) of a group 
-     * 
+     * Get members (users) of a group
+     *
      * @param realm
      * @param groupId
      * @param authorizationHeader
@@ -218,10 +227,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("groupId") UUID groupId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @SpringQueryMap PageRequest pageRequest);
-    
+
     /**
      * Get groups for a given user
-     * 
+     *
      * @param realm
      * @param userId
      * @param authorizationHeader
@@ -234,10 +243,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("realm") String realm,
         @PathVariable("userId") UUID userId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
-    
+
     /**
      * Create a group. The URI for the newly created group is returned a {@code location} response header.
-     * 
+     *
      * @param realm
      * @param authorizationHeader
      * @param group
@@ -250,10 +259,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("realm") String realm,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @RequestBody GroupDto group);
-   
+
     /**
      * Update a group
-     * 
+     *
      * @param realm
      * @param groupId
      * @param authorizationHeader
@@ -269,10 +278,10 @@ public interface KeycloakAdminFeignClient
         @PathVariable("groupId") UUID groupId,
         @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
         @RequestBody GroupDto group);
-    
+
     /**
      * Delete a group
-     * 
+     *
      * @param realm
      * @param groupId
      * @param authorizationHeader
