23543f35cc069cdbb706729ad03341c83b5bd1ed
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-09-28 10:46:41 +0300

    Update analytics model

--- src/main/java/eu/opertusmundi/common/model/analytics/TemporalDimension.java
+++ src/main/java/eu/opertusmundi/common/model/analytics/TemporalDimension.java
@@ -7,9 +7,15 @@ import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Setter
 public class TemporalDimension {
