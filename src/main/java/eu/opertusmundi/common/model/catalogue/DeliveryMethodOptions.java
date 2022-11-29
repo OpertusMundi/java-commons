54c82b0258999a453136603c1b2e14d5baae9d98
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-11-29 16:33:11 +0200

    Update validation constraints for delivery method options

--- src/main/java/eu/opertusmundi/common/model/catalogue/DeliveryMethodOptions.java
+++ src/main/java/eu/opertusmundi/common/model/catalogue/DeliveryMethodOptions.java
@@ -2,9 +2,8 @@ package eu.opertusmundi.common.model.catalogue;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -16,16 +15,16 @@ public class DeliveryMethodOptions implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Schema(description = "Type of physical media")
-    @NotEmpty
+    @Schema(description = "Type of physical media. Applicable only when delivery method is `PHYSICAL_PROVIDER`.", required = true)
+    @JsonInclude(Include.NON_EMPTY)
     private String mediaType;
 
     @Schema(description = "Notes for buyer")
+    @JsonInclude(Include.NON_EMPTY)
     private String notes;
 
-    @Schema(description = "Number of objects", minimum = "1")
-    @NotNull
-    @Min(1)
+    @Schema(description = "Number of objects. Applicable only when delivery method is `PHYSICAL_PROVIDER`", minimum = "1", required = true)
+    @JsonInclude(Include.NON_NULL)
     private Integer numberOfItems;
 
 }
