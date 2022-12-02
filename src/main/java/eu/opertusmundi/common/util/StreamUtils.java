3ba9be7ffed863b8b87e48d1c8db3edb80c26050
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-12-02 11:39:54 +0200

    Update Javadoc comments

--- src/main/java/eu/opertusmundi/common/util/StreamUtils.java
+++ src/main/java/eu/opertusmundi/common/util/StreamUtils.java
@@ -6,6 +6,14 @@ import java.util.stream.Stream;
 
 public class StreamUtils {
 
+    /**
+     * Creates a stream from a collection. If the collection is {@code null}, an
+     * empty stream is returned
+     * 
+     * @param <T>
+     * @param collection
+     * @return
+     */
     public static <T> Stream<T> from(Collection<T> collection) {
         return Optional.ofNullable(collection)
             .map(Collection::stream)
