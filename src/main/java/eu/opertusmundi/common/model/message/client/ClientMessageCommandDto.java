f43e87fd855743fb449a34f20e3bfee5a7c2d773
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-12-08 00:27:05 +0200

    Add static factory method to `ClientMessageCommandDto`

--- src/main/java/eu/opertusmundi/common/model/message/client/ClientMessageCommandDto.java
+++ src/main/java/eu/opertusmundi/common/model/message/client/ClientMessageCommandDto.java
@@ -2,11 +2,13 @@ package eu.opertusmundi.common.model.message.client;
 
 import javax.validation.constraints.NotEmpty;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
 @Getter
 @Setter
 public class ClientMessageCommandDto {
