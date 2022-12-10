e8abd3978090960f87d7b75eabd241c24ddb4a2a
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-12-10 16:34:42 +0200

    Add new workflow variable `bpmnErrorCode`

--- src/main/java/eu/opertusmundi/common/model/workflow/EnumProcessInstanceVariable.java
+++ src/main/java/eu/opertusmundi/common/model/workflow/EnumProcessInstanceVariable.java
@@ -2,9 +2,10 @@ package eu.opertusmundi.common.model.workflow;
 
 public enum EnumProcessInstanceVariable {
     START_USER_KEY("startUserKey"),
-    BPMN_BUSINESS_ERROR_DETAILS("bpmnBusinessErrorDetails"), 
+    BPMN_BUSINESS_ERROR_DETAILS("bpmnBusinessErrorDetails"),
     BPMN_BUSINESS_ERROR_MESSAGES("bpmnBusinessErrorMessages"),
     HELPDESK_ERROR_MESSAGE("helpdeskErrorMessage"),
+    BPMN_ERROR_CODE("bpmnErrorCode")
     ;
 
     private String value;
