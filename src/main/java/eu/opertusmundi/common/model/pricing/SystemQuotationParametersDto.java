1e3da545f43bb69c495aaba10bf7773018b81a4d
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2023-02-07 08:56:08 +0200

    Update quotation data model

--- src/main/java/eu/opertusmundi/common/model/pricing/SystemQuotationParametersDto.java
+++ src/main/java/eu/opertusmundi/common/model/pricing/SystemQuotationParametersDto.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -15,32 +14,39 @@ import lombok.ToString;
 @Schema(description = "Dynamic system-defined parameters set during the creation of a quotation such as the selected number of rows or population")
 @NoArgsConstructor
 @ToString
+@Getter
 public class SystemQuotationParametersDto implements Serializable {
 
     @Schema(description = "Tax applied based on the billing region")
-    @Getter
     protected BigDecimal taxPercent;
 
     private static final long serialVersionUID = 1L;
 
     @Schema(description = "System-defined parameter of the number of rows selected. If the pricing model does not support "
+                        + "number of rows parameter, this property is not set. This property will be replaced by `selectedRows`"
+                        + "property in future releases", deprecated = true)
+    @JsonInclude(Include.NON_NULL)
+    private Long rows;
+
+    @Schema(description = "System-defined parameter of the number of rows selected. If the pricing model does not support "
                         + "number of rows parameter, this property is not set")
     @JsonInclude(Include.NON_NULL)
-    @JsonProperty("rows")
-    @Getter
     private Long selectedRows;
 
     @Schema(description = "System-defined parameter of the total number of rows. If the pricing model does not support "
                         + "number of rows parameter, this property is not set")
     @JsonInclude(Include.NON_NULL)
-    @Getter
     private Long totalRows;
+
+    @Schema(description = "System-defined parameter of the size of selected population. If the pricing model does not "
+                        + "support population size parameter, this property is not set. This property will be replaced by "
+                        + "`selectedPopulation` property in future releases", deprecated = true)
+    @JsonInclude(Include.NON_NULL)
+    private Long population;
 
     @Schema(description = "System-defined parameter of the size of selected population. If the pricing model does not "
                         + "support population size parameter, this property is not set")
     @JsonInclude(Include.NON_NULL)
-    @JsonProperty("population")
-    @Getter
     private Long selectedPopulation;
 
     @Schema(description = "System-defined parameter of the total population. If the pricing model does not "
@@ -50,10 +56,14 @@ public class SystemQuotationParametersDto implements Serializable {
     private Long totalPopulation;
 
     @Schema(description = "System-defined parameter of the selected percent of total population. If the pricing model does not "
+                        + "support population size parameter, this property is not set. This property will be replaced by "
+                        + "`selectedPopulationPercent` property in future releases", deprecated = true)
+    @JsonInclude(Include.NON_NULL)
+    private Integer populationPercent;
+
+    @Schema(description = "System-defined parameter of the selected percent of total population. If the pricing model does not "
                         + "support population size parameter, this property is not set")
     @JsonInclude(Include.NON_NULL)
-    @JsonProperty("populationPercent")
-    @Getter
     private Integer selectedPopulationPercent;
 
     public static SystemQuotationParametersDto of(BigDecimal taxPercent) {
@@ -64,8 +74,9 @@ public class SystemQuotationParametersDto implements Serializable {
 
     public static SystemQuotationParametersDto ofRows(BigDecimal taxPercent, long selectedRows, long totalRows) {
         final SystemQuotationParametersDto p = new SystemQuotationParametersDto();
-        p.taxPercent   = taxPercent;
+        p.rows         = selectedRows;
         p.selectedRows = selectedRows;
+        p.taxPercent   = taxPercent;
         p.totalRows    = totalRows;
         return p;
     }
@@ -75,9 +86,11 @@ public class SystemQuotationParametersDto implements Serializable {
     ) {
 
         final SystemQuotationParametersDto p = new SystemQuotationParametersDto();
-        p.taxPercent                = taxPercent;
+        p.population                = selectedPopulation;
+        p.populationPercent         = selectedPopulationPercent;
         p.selectedPopulation        = selectedPopulation;
         p.selectedPopulationPercent = selectedPopulationPercent;
+        p.taxPercent                = taxPercent;
         p.totalPopulation           = totalPopulation;
         return p;
     }
