26517072e72e44e44efc9b95ff58760b77d012b8
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2023-02-11 23:47:58 +0200

    Remove non-PK FKs from ServiceUsageSummaryEntity (due to Hibernate bug)
    See https://stackoverflow.com/a/50328000/1943126

--- src/main/java/eu/opertusmundi/common/domain/ServiceUsageSummaryEntity.java
+++ src/main/java/eu/opertusmundi/common/domain/ServiceUsageSummaryEntity.java
@@ -5,15 +5,9 @@ import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
-import javax.persistence.PostLoad;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import org.hibernate.annotations.Immutable;
 
@@ -31,23 +25,7 @@ public class ServiceUsageSummaryEntity
     @Id
     @Column(name = "`service_key`", columnDefinition = "uuid")
     private UUID serviceKey;
-    
-    @MapsId("serviceKey")
-    @ManyToOne(fetch = FetchType.LAZY, optional = true)
-    @JoinColumn(name = "`service_key`", nullable = true, referencedColumnName = "`key`")
-    AccountSubscriptionEntity subscription;
-    
-    @Transient
-    private UUID subscriptionKey;
-    
-    @MapsId("serviceKey")
-    @ManyToOne(fetch = FetchType.LAZY, optional = true)
-    @JoinColumn(name = "`service_key`", nullable = true, referencedColumnName = "`key`")
-    private UserServiceEntity userService;
-    
-    @Transient
-    private UUID userServiceKey;
-    
+        
     @Id
     @Column(name = "`start_date`", columnDefinition = "date")
     private LocalDate startDate;
@@ -63,28 +41,10 @@ public class ServiceUsageSummaryEntity
     
     @Column(name = "`response_time_seconds`", columnDefinition = "numeric")
     private Float responseTimeInSeconds;
-
-    @PostLoad
-    void postLoad()
-    {
-        if (subscription != null) {
-            subscriptionKey = subscription.getKey();
-        }
-        
-        if (userService != null) {
-            userServiceKey = userService.getKey();
-        }
-    }
     
     public ServiceUsageSummaryDto toDto()
     {
         final ServiceUsageSummaryDto dto = new ServiceUsageSummaryDto(serviceKey, startDate);
-
-        if (subscriptionKey != null) {
-            dto.setSubscription(subscription.toConsumerDto(false));
-        } else if (userServiceKey != null) {
-            dto.setUserService(userService.toDto(false));
-        }
         
         dto.setCalls(calls);
         dto.setCallsNormalized(callsNormalized);
--- src/main/java/eu/opertusmundi/common/model/account/ServiceUsageSummaryDto.java
+++ src/main/java/eu/opertusmundi/common/model/account/ServiceUsageSummaryDto.java
@@ -3,19 +3,23 @@ package eu.opertusmundi.common.model.account;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import eu.opertusmundi.common.model.asset.service.UserServiceDto;
 
 @lombok.Getter
 @lombok.Setter
 @lombok.ToString(callSuper = true)
+@JsonInclude(value = Include.NON_NULL)
 public class ServiceUsageSummaryDto extends ServiceUsageKey
 {
+    private static final long serialVersionUID = 1L;
+    
     public ServiceUsageSummaryDto(UUID serviceKey, LocalDate startDate)
     {
         super(serviceKey, startDate);
     }
-
-    private static final long serialVersionUID = 1L;
 
     AccountSubscriptionDto subscription;
     
